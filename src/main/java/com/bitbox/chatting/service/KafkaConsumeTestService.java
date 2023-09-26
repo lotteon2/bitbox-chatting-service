555fc3f2350f7c0ac7b9e92eaeee5ebac428c84c
최성훈 <33412452+realsuperman@users.noreply.github.com>
2023-09-26 12:33:20 +0900

    [FIX] - 카프카 consume 연동 관련 예제 코드 추가(객체 직렬화 관련)

--- src/main/java/com/bitbox/chatting/service/KafkaConsumeTestService.java
+++ src/main/java/com/bitbox/chatting/service/KafkaConsumeTestService.java
@@ -3,38 +3,25 @@ package com.bitbox.chatting.service;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import io.github.bitbox.bitbox.dto.NotificationDto;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 
 @Service
 @Slf4j
 public class KafkaConsumeTestService {
     @KafkaListener(topics = "attendanceAlarm")
-    public void updateQty(String kafkaMessage) {
+    public void kafkaTopicTest(String kafkaMessage) {
         ObjectMapper mapper = new ObjectMapper();
-        List<NotificationDto> notificationDtoList;
+        NotificationDto notificationDto;
         try {
-            notificationDtoList = mapper.readValue(kafkaMessage, new TypeReference<>() {});
+            notificationDto = mapper.readValue(kafkaMessage, new TypeReference<>() {});
         } catch (JsonProcessingException ex) {
             throw new RuntimeException(ex);
         }
 
-        for (NotificationDto notificationDto : notificationDtoList) {
-            log.info("memberId = {}", notificationDto.getMemberId());
-        }
-
+        log.info("memberId = {}",notificationDto.getNotificationType());
     }
-}
-
-@Getter
-@NoArgsConstructor
-class NotificationDto{
-    private long memberId;
-    private String messageType;
-    private String message;
 }
