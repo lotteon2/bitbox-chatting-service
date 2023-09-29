b14ab76bf81fc574152359ad1cd2501afc0556bb
최성훈 <33412452+realsuperman@users.noreply.github.com>
2023-09-29 21:36:47 +0900

    [FEAT] - SAGA 보상 패턴 예제 코드 작성 - 카프카 연동 예제 코드 작성

--- src/main/java/com/bitbox/chatting/controller/TestController.java
+++ src/main/java/com/bitbox/chatting/controller/TestController.java
@@ -1,7 +1,5 @@
 package com.bitbox.chatting.controller;
 
-import lombok.Builder;
-import lombok.Getter;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
--- src/main/java/com/bitbox/chatting/service/KafkaConsumeTestService.java
+++ src/main/java/com/bitbox/chatting/service/KafkaConsumeTestService.java
@@ -3,25 +3,37 @@ package com.bitbox.chatting.service;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import io.github.bitbox.bitbox.dto.NotificationDto;
+import io.github.bitbox.bitbox.dto.MemberPaymentDto;
+import io.github.bitbox.bitbox.util.KafkaProducer;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 
 @Service
 @Slf4j
+@RequiredArgsConstructor
 public class KafkaConsumeTestService {
-    @KafkaListener(topics = "attendanceAlarm")
+    private final KafkaTemplate<String, Object> kafkaTemplate;
+
+    @KafkaListener(topics = "memberCredit")
     public void kafkaTopicTest(String kafkaMessage) {
         ObjectMapper mapper = new ObjectMapper();
-        NotificationDto notificationDto;
+        MemberPaymentDto memberPaymentDto;
         try {
-            notificationDto = mapper.readValue(kafkaMessage, new TypeReference<>() {});
+            memberPaymentDto = mapper.readValue(kafkaMessage, new TypeReference<>() {});
         } catch (JsonProcessingException ex) {
             throw new RuntimeException(ex);
         }
 
-        log.info("memberId = {}",notificationDto.getNotificationType());
+        log.info("memberId = {}",memberPaymentDto.getMemberId());
+        log.info("credit = {}",memberPaymentDto.getMemberCredit());
+        log.info("tid = {}",memberPaymentDto.getTid());
+        log.info("cancelAmount = {}",memberPaymentDto.getCancelAmount());
+        log.info("cancelTaxFreeAmount = {}",memberPaymentDto.getCancelTaxFreeAmount());
+
+        KafkaProducer.send(kafkaTemplate, "kakaopayCancel", memberPaymentDto);
     }
 }
