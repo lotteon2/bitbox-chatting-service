fe6a28f6313cf098402d462e6a56152d641335fb
최성훈 <33412452+realsuperman@users.noreply.github.com>
2023-10-08 20:02:25 +0900

    [FEAT] - validation 추가

--- src/test/java/com/bitbox/chatting/service/ChattingServiceTest.java
+++ src/test/java/com/bitbox/chatting/service/ChattingServiceTest.java
@@ -1,7 +1,13 @@
 package com.bitbox.chatting.service;
 
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
+@Transactional
 class ChattingServiceTest {
+    // KafkaListenerClass의 kafkaTopicTest테스트 해야함
 
 }
