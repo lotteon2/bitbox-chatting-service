3667e19315a89c5803a85827635eaf848a086ae4
최성훈 <33412452+realsuperman@users.noreply.github.com>
2023-10-18 20:55:38 +0900

    [FEAT] - Feign 헤더 추가

--- src/main/java/com/bitbox/chatting/feign/FeignServiceClient.java
+++ src/main/java/com/bitbox/chatting/feign/FeignServiceClient.java
@@ -1,22 +1,25 @@
 package com.bitbox.chatting.feign;
 
-import com.bitbox.chatting.config.FeignServiceClientConfig;
 import com.bitbox.chatting.dto.SubscriptionResponseDto;
+import feign.Headers;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "apigateway-service", configuration = FeignServiceClientConfig.class)
+@FeignClient(name = "apigateway-service")
 public interface FeignServiceClient {
     @GetMapping("/payment-service/member/subscription")
+    @Headers("Authorization")
     SubscriptionResponseDto getSubscription();
 
     @GetMapping("/payment-service/member/{memberId}/subscription")
+    @Headers("Authorization")
     SubscriptionResponseDto getSubscription(@PathVariable String memberId);
 
     @PatchMapping("/user-service/member/credit")
+    @Headers("Authorization")
     ResponseEntity<Void> updateMemberCredit(int credit);
 
 }
